442ec54633dad83093768b198fc0c29d0dd2f0c0
esdalee <es.da.lee@gmail.com>
2019-05-19 22:08:47 -0400

    pseudo code Trivia game logic

---
+++ assets/javascript/app.js
@@ -0,0 +1,62 @@
+$(document).ready(function() {
+
+    // Nested objects for trivia Q&As
+    var trivia = {
+        q1: {
+
+        } 
+// What pot of food does Kevin spill in Casual Friday episode?
+// Chili
+        }
+        q2: {
+// Whose stand up routine does michael do in Diversity Day?
+// Chris Rock
+        }
+        q3: {
+// How many siblings does Dwight have?
+// 1 brother, 1 sister
+        }
+        q4: {
+// What is the name of the movie Michael Scott wrote and directed
+// Thread Level Midnight
+        }
+        q5: {
+// What is the name of the cat that Andy gets for Angela after she breaks up with Dwight?
+// Garbage/Bandit
+        }
+        q6: {
+// Angela and Pam both have a son. They both named their son what name?
+// Phillip
+        }
+        q7: {
+// What is the name of Oscar's first boyfriend?
+// Gil
+        }
+        q8: {
+// What food does Ryan burn, causing an office fire?
+// Cheesy pita
+        }
+        q9: {
+// Who does Michael run over with his car?
+// Meredith
+        }
+        q10: {
+
+        }
+    }
+
+    // Click handler for start button
+
+    // Set countdown function
+
+    // Once start button is clicked, timer is set off
+
+    // * Only one question can be selected
+
+    // Timer runs out - check answers
+
+    // Reveal number of correct and incorrect answers
+
+
+
+});
