584ad6af9e9e5bc1f1d2202e2456871d8e0ec98e
esdalee <es.da.lee@gmail.com>
2019-05-21 22:33:22 -0400

    make trivia object

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,52 +1,51 @@
 $(document).ready(function() {
 
-var triviaQ = ["What Pot of food of food does Kevin spill in Casual Friday episode?", "How many siblings does Dwight have?", "What is the name of the movie Michael wrote and directed?", "What is the name of the cat Andy gets for Angela after she breaks up with Dwight?", "What is the name of Oscar's first boyfriend?", "What food does Ryan burn, causing an office fire?", "Who does Michael run over with his car?", "What did Kelly do to lose weight for the Dunder Mifflin weight loss contest?", "What is Stanley's life goal and dream?"];
+// Initialize variables
 
-    // Nested objects for trivia Q&As
-    var trivia = {
-        q1: {
 
-        } 
-// What pot of food does Kevin spill in Casual Friday episode?
-// Chili
-        }
-        q2: {
 
-// How many siblings does Dwight have?
-// 1 brother, 1 sister
-        }
-        q4: {
-// What is the name of the movie Michael Scott wrote and directed
-// Thread Level Midnight
-        }
-        q5: {
-// What is the name of the cat that Andy gets for Angela after she breaks up with Dwight?
-// Garbage/Bandit
-        }
-        q6: {
+var numCorrect = 0;
 
-        }
-        q7: {
-// What is the name of Oscar's first boyfriend?
-// Gil
-        }
-        q8: {
-// What food does Ryan burn, causing an office fire?
-// Cheesy pita
-        }
-        q9: {
-// Who does Michael run over with his car?
-// Meredith
-        }
-        q10: {
+// Nested objects for trivia Q&As
 
+var trivia = {
+        questions: {
+                q1: "What Pot of food of food does Kevin spill in Casual Friday ep?",
+                q2: "How many siblings does Dwight have?",
+                q3: "What is the name of the movie Michael wrote and directed?",
+                q4: "What is the name of the cat Andy gets for Angela after she breaks up with Dwight?",
+                q5: "What is the name of Oscar's first boyfriend?",
+                q6: "What food does Ryan burn, causing an office fire?",
+                q7: "Who does Michael run over with his car?",
+                q8: "What did Kelly do to lose weight for the Dunder Mifflin weight loss contest?",
+                q9: "What is Stanley's life goal and dream?",
+                q10: "Whose stand up routine does Michael do in Diversity Day ep?"
+        },
+        choices: {
+                q1: ["Chilli","Chicken Noodle Soup", "Fried Rice", "Cereal"],
+                q2: ["None", "1 Brother & 1 Sister", "5 Sisters", "1 Brother"],
+                q3: ["Dundy Miffy", "Thread Level Midnight", "Michael Scott Show", "World's Best Boss"],
+                q4: ["Pundit", "Andela", "Garbage/Bandit", "Trash"],
+                q5: ["Gil", "Dwight", "Creed", "Bill"],
+                q6: ["Lasagna", "Cheesy Pita", "Burrito", "Creme Brule"],
+                q7: ["Jim", "Meredith", "Pam", "Jan"],
+                q8: ["Starve", "Keto-Diet", "Eat a Tapeworm", "Eat Dirt"],
+                q9: ["Quit", "To Own a Lighthouse and Live at the Top of It", "Have 50 Grandchildren", "Be a Supermodel"],
+                q10: ["Jerry Seinfeld", "Chris Rock", "Dave Chappelle", "Ali Wong"]
+        },
+        answers: {
+                q1: "Chilli",
+                q2: "1 Brother & 1 Sister",
+                q3: "Thread Level Midnight",
+                q4: "Garbage/Bandit",
+                q5: "Gil",
+                q6: "Cheesy Pita",
+                q7: "Meredith",
+                q8: "Eat a Tapeworm",
+                q9: "To own a lighthouse and live at the top of it",
+                q10: "Chris Rock"             
         }
-    }
-// What did Kelly do to lose weight for the Dunder Mifflin weight loss contest?
-// Eat a tapeworm
-
-// What is Stanley's life goal and dream
-// To own a lighthouse and live at the top of it
+}
 
 
     // Click handler for start button
@@ -55,11 +54,18 @@ var triviaQ = ["What Pot of food of food does Kevin spill in Casual Friday episo
 
     // Once start button is clicked, timer is set off
 
+
     // * Only one question can be selected
 
     // Timer runs out - check answers
 
+// function check(){
+//         if ()
+// }
+
     // Reveal number of correct and incorrect answers
+
+
 
 
 
